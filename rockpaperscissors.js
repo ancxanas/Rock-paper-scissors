b5281bf0df07e00cfc445c9eea4a3d350bc6c66b
ancxanas <anas.corona.github@gmail.com>
2022-10-04 16:50:33 +0530

    added playRound()

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,5 +1,18 @@
+//computer choice
 function getComputerChoice(){
-    let string = ['Rock','Paper','Scissors'];
-    let random = Math.floor(Math.random()*string.length);
-    return string[random];
+    let string = ['Rock','Paper','Scissors']; // string declaration
+    let random = Math.floor(Math.random()*string.length); // selecting an array from string
+    return string[random]; // returning string
+}
+//Single Round of Rock Paper Scissors
+function playRound(playerSelection,computerSelection){
+    if(playerSelection < computerSelection){
+        return (`You Lose! ${computerSelection} beats ${playerSelection} `);
+    }
+    else if (playerSelection > computerSelection){
+        return (`You Win ${playerSelection} beats ${computerSelection} `)
+    }
+    else{
+        return ('It\'s a Tie')
+    }
 }
