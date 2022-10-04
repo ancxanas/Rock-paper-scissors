697ead753170f789258c0d44256bb64035265aba
ancxanas <anas.corona.github@gmail.com>
2022-10-04 17:42:32 +0530

    Restart

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -4,15 +4,3 @@ function getComputerChoice(){
     let random = Math.floor(Math.random()*string.length); // selecting an array from string
     return string[random]; // returning string
 }
-//Single Round of Rock Paper Scissors
-function playRound(playerSelection,computerSelection){
-    if(playerSelection < computerSelection){
-        return (`You Lose! ${computerSelection} beats ${playerSelection} `);
-    }
-    else if (playerSelection > computerSelection){
-        return (`You Win ${playerSelection} beats ${computerSelection} `)
-    }
-    else{
-        return ('It\'s a Tie')
-    }
-}
