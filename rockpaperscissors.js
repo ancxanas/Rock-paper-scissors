d5bef909224e3ee048d15001aa5c8fbef0abfaea
ancxanas <anas.corona.github@gmail.com>
2022-10-05 12:58:31 +0530

    Added scoring

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,42 +1,60 @@
+let computerScore = 0;
+let playerScore = 0;
+let draw = 0;
+game();
+
 //computer choice
 function getComputerChoice(){
     let string = ['Rock','Paper','Scissors']; // string declaration
     let random = Math.floor(Math.random()*string.length); // selecting an array from string
     return string[random]; // returning string
 }
+
 //Single round of Rock paper Scissors
 function playRound(playerSelection,computerSelection){
     if (playerSelection === computerSelection){
         console.log('It\'s a Tie');
+        draw++;
     }
     else if (playerSelection.toLowerCase() === 'rock'){
         if (computerSelection === 'Paper'){
-            return(`You Lose! ${computerSelection} beats ${playerSelection}`)
+            console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+            computerScore++;
         }
         else{
-            return(`You Win! ${playerSelection} beats ${computerSelection}`)
+            console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+            playerScore++;
         }
     }
     else if (playerSelection.toLowerCase() === 'scissors'){
         if (computerSelection === 'Rock'){
-            return(`You Lose! ${computerSelection} beats ${playerSelection}`)
+            console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+            computerScore++;
         }
         else{
-            return(`You Win! ${playerSelection} beats ${computerSelection}`)
+            console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+            playerScore++;
         }
     }
     else if (playerSelection.toLowerCase() === 'paper'){
         if (computerSelection === 'Scissors'){
-            return(`You Lose! ${computerSelection} beats ${playerSelection}`)
+            console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+            computerScore++;
         }
         else{
-            return(`You Win! ${playerSelection} beats ${computerSelection}`)
+            console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+            playerScore++;
         }
     }
     else{
-        return('Type Correctly');
+        console.log('Type Correctly');
     }
 }
+
+
+
+
+
 
 //game
 function game(){
@@ -44,6 +62,11 @@ function game(){
         const playerSelection = prompt('what\'s your choice');
         const computerSelection = getComputerChoice();
         console.log(playRound(playerSelection,computerSelection));
-    }
+        console.log(`Computer ${computerScore} : Player ${playerScore}`)
+        }
 }
-game();
+
+
+
+
+
