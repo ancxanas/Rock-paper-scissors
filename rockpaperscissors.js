1b75981c930f8f5c4164fdd07e1a99f788868936
ancxanas <anas.corona.github@gmail.com>
2022-10-14 17:41:48 +0530

    Added functions to hide and show buttons Show scores and winner if a player reaches the score 5

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,78 +1,142 @@
 let computerScore = 0;
 let playerScore = 0;
 let draw = 0;
-game();
+
 
 //computer choice
 function getComputerChoice(){
-    let string = ['Rock','Paper','Scissors']; // string declaration
-    let random = Math.floor(Math.random()*string.length); // selecting an array from string
-    return string[random]; // returning string
+    let string = ['Rock','Paper','Scissors'];
+    let random = Math.floor(Math.random()*string.length);
+    return string[random];
 }
 
 //Single round of Rock paper Scissors
-function playRound(playerSelection,computerSelection){
-    if (playerSelection === computerSelection){
-        console.log('It\'s a Tie');
+function playRound(playerSelection){
+    computerSelection = getComputerChoice();
+    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()){
+        document.getElementById('game-result').innerHTML = ('It\'s a Tie');
         draw++;
+        document.getElementById('player-score').innerHTML = (`Player : ${playerScore}`);
+        document.getElementById('computer-score').innerHTML = (`Computer : ${computerScore}`);
     }
     else if (playerSelection.toLowerCase() === 'rock'){
         if (computerSelection === 'Paper'){
-            console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+            document.getElementById('game-result').innerHTML = (`You Lose! ${computerSelection} beats ${playerSelection}`);
             computerScore++;
+            document.getElementById('player-score').innerHTML = (`Player : ${playerScore}`);
+            document.getElementById('computer-score').innerHTML = (`Computer : ${computerScore}`);
         }
         else{
-            console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+            document.getElementById('game-result').innerHTML = (`You Win! ${playerSelection} beats ${computerSelection}`);
             playerScore++;
+            document.getElementById('player-score').innerHTML = (`Player : ${playerScore}`);
+            document.getElementById('computer-score').innerHTML = (`Computer : ${computerScore}`);
         }
     }
     else if (playerSelection.toLowerCase() === 'scissors'){
         if (computerSelection === 'Rock'){
-            console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+            document.getElementById('game-result').innerHTML = (`You Lose! ${computerSelection} beats ${playerSelection}`);
             computerScore++;
+            document.getElementById('player-score').innerHTML = (`Player : ${playerScore}`);
+            document.getElementById('computer-score').innerHTML = (`Computer : ${computerScore}`);
         }
         else{
-            console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+            document.getElementById('game-result').innerHTML = (`You Win! ${playerSelection} beats ${computerSelection}`);
             playerScore++;
+            document.getElementById('player-score').innerHTML = (`Player : ${playerScore}`);
+            document.getElementById('computer-score').innerHTML = (`Computer : ${computerScore}`);
         }
     }
     else if (playerSelection.toLowerCase() === 'paper'){
         if (computerSelection === 'Scissors'){
-            console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+            document.getElementById('game-result').innerHTML = (`You Lose! ${computerSelection} beats ${playerSelection}`);
             computerScore++;
+            document.getElementById('player-score').innerHTML = (`Player : ${playerScore}`);
+            document.getElementById('computer-score').innerHTML = (`Computer : ${computerScore}`);
         }
         else{
-            console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+            document.getElementById('game-result').innerHTML = (`You Win! ${playerSelection} beats ${computerSelection}`);
             playerScore++;
+            document.getElementById('player-score').innerHTML = (`Player : ${playerScore}`);
+            document.getElementById('computer-score').innerHTML = (`Computer : ${computerScore}`);
         }
-    }
-    else{
-        console.log('Type Correctly');
     }
 }
 
 
+//Player Selection in buttons
+const rockButton = document.querySelector('#buttonRock');
+const paperButton = document.querySelector('#buttonPaper');
+const scissorsButton = document.querySelector('#buttonScissors');
+const resetButton = document.querySelector('#buttonReset');
 
 
+//EventListeners for buttons
+rockButton.addEventListener('click',() => {
+    game('rock');
+});
+paperButton.addEventListener('click',() => {
+    game('paper');
+})
+scissorsButton.addEventListener('click',() => {
+    game('scissors')
+})
 
+//resetbutton resets score
+resetButton.addEventListener('click', ()=>{
+    computerScore = 0;
+    playerScore = 0;
+    document.getElementById('player-score').innerHTML = (`Player : ${playerScore}`);
+    document.getElementById('computer-score').innerHTML = (`Computer : ${computerScore}`);
+    showGameButtons();
+    hideWinnerMessage();
+})
 
-//game
-function game(){
-    for (let i = 0; i < 5; i++){
-        const playerSelection = prompt('what\'s your choice');
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection,computerSelection));
-        console.log(`Computer ${computerScore} : Player ${playerScore}`)
-        }
-        if(playerScore > computerScore){
-            console.log('Player Wins the Round')
-        } 
-        else{
-            console.log('Computer Wins the Round')
-        }
+//game Round
+function game(playerSelection){
+    computerSelection = getComputerChoice();
+    playRound(playerSelection,computerSelection);
+
+    if(computerScore == 5){
+        document.getElementById('winner-message').innerHTML = (`Computer Wins`);
+        hideGameButtons();
+        showWinnerMessage();
+    }
+    if(playerScore == 5){
+        document.getElementById('winner-message').innerHTML = (`Player Wins`);
+        hideGameButtons();
+        showWinnerMessage();
+    }
 }
 
+// Hides game buttons
+function hideGameButtons(){
+    document.getElementById('buttonRock').style.visibility = 'hidden';
+    document.getElementById('buttonRock').style.display = 'none';
+    document.getElementById('buttonPaper').style.visibility = 'hidden';
+    document.getElementById('buttonPaper').style.display = 'none';
+    document.getElementById('buttonScissors').style.visibility = 'hidden';
+    document.getElementById('buttonScissors').style.display = 'none';
+}
 
+// Show game buttons 
+function showGameButtons(){
+    document.getElementById('buttonRock').style.visibility = 'visible';
+    document.getElementById('buttonRock').style.display = 'inline';
+    document.getElementById('buttonPaper').style.visibility = 'visible';
+    document.getElementById('buttonPaper').style.display = 'inline';
+    document.getElementById('buttonScissors').style.visibility = 'visible';
+    document.getElementById('buttonScissors').style.display = 'inline';
+}
 
+// Hide winner message
+function hideWinnerMessage(){
+    document.getElementById('winner-message').style.visibility = 'hidden';
+    document.getElementById('winner-message').style.display = 'none';
+}
 
-
+//Show winner message
+function showWinnerMessage(){
+    document.getElementById('winner-message').style.visibility = 'visible';
+    document.getElementById('winner-message').style.display = 'block';
+}
